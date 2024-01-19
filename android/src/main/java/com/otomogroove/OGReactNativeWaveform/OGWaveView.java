7835ec7e6be71df07782868fda6733bb52a11ff7
Bigpawn <shangshang86799@163.com>
2024-01-19 20:27:58 +0800

    安卓报错修复

--- android/src/main/java/com/otomogroove/OGReactNativeWaveform/OGWaveView.java
+++ android/src/main/java/com/otomogroove/OGReactNativeWaveform/OGWaveView.java
@@ -241,8 +241,11 @@ public class OGWaveView extends FrameLayout {
         protected void onPostExecute(Float aFloat) {
             super.onPostExecute(aFloat);
 
-
-            mUIWave.updatePlayHead(aFloat);
+            if(aFloat != null) {
+                mUIWave.updatePlayHead(aFloat);
+            } else {
+                Log.e("XSXGOT","aFloat is null");
+            }
         }
     }
 
